7a69c2fb876f3dad807aeb34b6083a4eaae42dd3
Nakshatra bansal <75303956+naksh2004@users.noreply.github.com>
2024-05-27 14:46:34 +0530

    Delete text_processor.py

--- text_processor.py
+++
@@ -1,8 +0,0 @@
-# text_processor.py
-
-from fastapi import UploadFile
-
-async def process_text(file: UploadFile):
-    content = await file.read()
-    text = content.decode('utf-8')
-    return text
